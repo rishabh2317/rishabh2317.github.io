f2a4d3bcc3f3c911b927909bcc4440648cf26c5c
rishabh2317 <rishabh.thakur2317@gmail.com>
2021-11-15 15:05:00 +0530

    Add files via upload

---
+++ api.php
@@ -0,0 +1,222 @@
+<?php
+
+$accesskey = 'ak_live_32xEwQT0COE8t81XvBBD76WzJ2juj2mV7hcz';
+$secretkey = 'sk_live_8zC9y3L1Zl4zOk2XDKvJ932fpLVUxEu5gWhv';
+
+
+ if(isset($_POST['id'])){
+
+                      
+  $api = new Api($accesskey,$secretkey);
+  $balance_data = $api->get_balance($_POST['id']);
+  print_r($balance_data);
+
+                      
+}
+
+Class Api{
+
+    const BASE_URL_UAT = "https://api.zwitch.io/v1";
+
+    public function __construct($access_key,$secret_key){
+
+        $this->access_key = $access_key;
+        $this->secret_key = $secret_key;
+    }
+
+    public function create_account($data){
+
+        try {
+           
+           
+           if( $data['kyc']['state_code'] != NULL){
+            $request_data = array(
+            'name'   			=> (!empty($data['name']))? $data['name'] : NULL,
+            'mobile_number' 	=> (!empty($data['mobile_number']))? $data['mobile_number'] : NULL,
+            'email'     			=> (!empty($data['email']))? $data['email'] : NULL,
+            'type' 			=> (!empty($data['type']))? $data['type'] : NULL,
+            'used_as' 	=> (!empty($data['used_as']))?  $data['used_as'] : NULL,
+            'create_vpa' 	=> (!empty($data['create_vpa']))?  $data['create_vpa'] : NULL,
+            'kyc'   => (object)array(
+            'business_type' =>  (!empty($data['kyc']['business_type']))?  $data['kyc']['business_type'] : NULL,
+            'state_code' =>  (!empty($data['kyc']['state_code']))?  $data['kyc']['state_code'] : NULL,
+            'business_category' =>  (!empty($data['kyc']['business_category']))?  $data['kyc']['business_category'] : NULL,
+            'city' => (!empty($data['kyc']['city']))?  $data['kyc']['city'] : NULL,
+            'postal_code' => (!empty($data['kyc']['postal_code']))?  $data['kyc']['postal_code'] : NULL,
+            'pan' => (!empty($data['kyc']['pan']))?  $data['kyc']['pan'] : NULL,
+              ),  
+
+            );
+        }else{
+
+            $request_data = array(
+            'name'              => (!empty($data['name']))? $data['name'] : NULL,
+            'mobile_number'     => (!empty($data['mobile_number']))? $data['mobile_number'] : NULL,
+            'email'                 => (!empty($data['email']))? $data['email'] : NULL,
+            'type'          => (!empty($data['type']))? $data['type'] : NULL,
+            'used_as'   => (!empty($data['used_as']))?  $data['used_as'] : NULL,
+            'kyc'   => (object)array(
+                'business_category' =>  (!empty($data['kyc']['business_category']))?  $data['kyc']['business_category'] : NULL,
+            ),  
+        );
+
+
+        }
+
+            $accounts_response = $this->http_post($request_data,"accounts");
+
+            return $accounts_response;
+        } catch (Exception $e){			
+            return [
+                'error' => $e->getMessage()
+            ];
+
+        } catch (Throwable $e){
+			
+			return [
+                'error' => $e->getMessage()
+            ];
+        }
+    }
+
+
+    public function get_balance($data){
+
+        try {
+           
+
+            $balance_response = $this->get_accounts_balance("accounts/".$data."/balance");
+            return $balance_response;
+        } catch (Exception $e){         
+            return [
+                'error' => $e->getMessage()
+            ];
+
+        } catch (Throwable $e){
+            
+            return [
+                'error' => $e->getMessage()
+            ];
+        }
+    }
+
+
+
+
+
+
+
+    
+    function build_auth(){
+
+        return array(                       
+            'Content-Type: application/json',                                 
+            'Authorization: Bearer '.$this->access_key.':'.$this->secret_key            
+        );
+
+    }
+
+
+    function http_post($data,$route){
+
+        $data = array_filter($data);
+
+        // foreach (@$data as $key=>$value){
+
+        //     if(empty($data[$key])){
+
+        //         unset($data[$key]);
+        //     }
+        // }
+ 
+        $url = self::BASE_URL_UAT."/".$route;
+       
+        $header = $this->build_auth();
+        
+        try
+        {
+            
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => $url,
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "POST",
+              CURLOPT_POSTFIELDS => json_encode($data),
+              CURLOPT_HTTPHEADER =>$header,
+            ));
+
+            $response = curl_exec($curl);
+            
+            curl_close($curl);
+            
+            return json_decode($response,true);
+        }
+        catch(Exception $e)
+        {
+            return [
+                "error" => "Http Post failed.",
+                "error_data" => $e->getMessage(),
+            ];
+        }           
+        
+    }
+
+
+
+    function get_accounts_balance($route){
+
+     
+        $url = self::BASE_URL_UAT."/".$route;
+       
+        $header = $this->build_auth();
+        
+        try
+        {
+            
+            $curl = curl_init();
+
+            curl_setopt_array($curl, array(
+              CURLOPT_URL => $url,
+              CURLOPT_RETURNTRANSFER => true,
+              CURLOPT_ENCODING => "",
+              CURLOPT_MAXREDIRS => 10,
+              CURLOPT_TIMEOUT => 0,
+              CURLOPT_FOLLOWLOCATION => true,
+              CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+              CURLOPT_CUSTOMREQUEST => "GET",
+              CURLOPT_HTTPHEADER =>$header,
+            ));
+
+            $response = curl_exec($curl);
+
+            curl_close($curl);
+            // $curlerr = curl_error($curl);
+        
+            
+            // if($curlerr != '')
+            // {
+            //     return [
+            //         "error" => "Http Post failed.",
+            //         "error_data" => $curlerr,
+            //     ];
+            // }
+            return $response;
+        }
+        catch(Exception $e)
+        {
+            return [
+                "error" => "Http Post failed.",
+                "error_data" => $e->getMessage(),
+            ];
+        }           
+        
+    }
+
+   
+}
---
+++ index.php
@@ -0,0 +1,214 @@
+<?php
+session_start();
+ob_start();
+
+
+require_once 'api.php';
+
+
+$accesskey = 'ak_live_32xEwQT0COE8t81XvBBD76WzJ2juj2mV7hcz';
+$secretkey = 'sk_live_8zC9y3L1Zl4zOk2XDKvJ932fpLVUxEu5gWhv';
+
+
+
+$fullname = $_POST['name'];
+$mobile_number = $_POST['mobile_number'];
+$email = $_POST['email'];
+$type = "virtual";
+$used_as ="collection_tool";
+$create_vpa = true;
+$state_code = 'KA';
+$city = 'Bangalore';
+$postal_code = 683101;
+$business_category = 'agri_business';
+$business_type = 'individual';
+$pan = 'EQKPM7093F';
+
+
+
+$data = [
+                'name' => $fullname,
+                'mobile_number' => $mobile_number,
+                'email'  => $email,
+                'type' => $type,
+                'used_as' => $used_as,
+                'create_vpa' => $create_vpa,
+                'kyc' => [
+                'state_code' => $state_code,
+                'business_category' => $business_category,
+                'city' => $city,
+				'business_type' => $business_type,
+                'postal_code' => $postal_code,
+                'pan' => $pan
+                  ],
+         ];
+
+
+
+
+//main logic
+
+$api = new Api($accesskey,$secretkey);
+$response_data = $api->create_account($data);
+   
+
+
+?>
+
+<script type="text/javascript">
+
+function getbalance(self) {
+
+	//console.log($(self).data('accountsId'))
+	var id = $(self).data('accountsId')
+	
+	$.ajax({  
+				type: "POST",  
+				url: "api.php?get_balance=true",  
+				data: {
+					"_token": "{{ csrf_token() }}",
+					'id':id,
+					
+				},  
+				success: function(datas) 
+				{ 
+					
+
+					var response=JSON.parse(datas);
+					$("#balance").html(response.amount);
+					
+
+					
+				}
+			});
+
+}
+
+
+ </script>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>ZWITCH ACCOUNTS</title>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+
+<script src="<?php echo $remote_script; ?>"></script>
+
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" >
+
+</head>
+<style>
+input[type=text], select {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  display: inline-block;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-sizing: border-box;
+}
+
+button {
+    box-sizing: border-box;
+    min-height: 40px;
+    min-width: 115px;
+    border-radius: 8px;
+    font-size: 14px;
+    font-weight: 500;
+    padding: 0 35px;
+    border: none;
+    cursor: pointer;
+    background: #4236ff 0 0 no-repeat padding-box;
+    box-shadow: 0 0 20px 0 rgb(0 0 0 / 20%);
+    color: #fff;
+    margin: auto;
+    display: inherit;
+}
+
+input[type=button]:hover {
+  background-color: #45a049;
+}
+
+div {
+  border-radius: 5px;
+  background-color: #000000;
+  padding: 20px;
+}
+label{
+	color: #c7c7c7
+}
+
+body{
+  font-family: sans-serif;
+}
+
+strong{
+	color: #4236ff;
+	min-width: 200px;
+	display: inline-block;
+}
+
+h3{
+	color: #c7c7c7
+}
+
+</style>
+<body>
+<div style="width: 600px;margin: auto;">
+<h3> <center> Virtual Account Demo </span> </center></h3>
+
+	<div class="dv">
+		<label> <strong> Type: </strong> <?php echo $response_data['type']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong>Name: </strong> <?php echo $data['name']; ?></label>
+	</div>
+	<div class="dv">
+		<label> <strong> E-mail: </strong> <?php echo $data['email']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong> Mobile Number: </strong><?php echo $data['mobile_number']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong> Bank Name: </strong> <?php echo $response_data['bank_name']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong> Account Number: </strong> <?php echo $response_data['account_number']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong> IFSC Code: </strong> <?php echo $response_data['ifsc_code']; ?></label>
+	</div>
+
+	<div class="dv">
+		<label> <strong>  VPA: </strong> <?php echo $response_data['vpa']; ?></label>
+	</div>
+
+    <div class="dv">
+    	<label> <strong>  QR: </strong></label>
+		<img src="qr.png" style="    width: 10%;
+    position: relative;">
+	</div>
+	
+
+	
+	<div class="dv">
+		<label> <strong> Account Balance: </strong> <span id="balance"> 0 </span>  </label> 
+	</div>
+
+	
+	<div class='dv'><button id="get_balance_btn" type='button' onclick='getbalance(this)' data-accounts-id=<?= $response_data['id']?> >Update Balance</button></div>
+
+	
+</div>
+</body>
+</html>
+
